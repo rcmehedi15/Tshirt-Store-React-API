e78d5c9e4d681fba5807a0c9c3e36a38be154610
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-04-08 15:55:13 +0600

    api data fetching finish

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const tshirts = useLoaderData();
     return (
         <div>
-            <h1> Hello Home</h1>
+            <h1> Hello Home, T shirt {tshirts.length}</h1>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,7 @@ const router = createBrowserRouter([
      {
       path :'/',
       element: <Home></Home>,
+      loader: () => fetch('tshirts.json')
      },
      {
       path :'/review',
